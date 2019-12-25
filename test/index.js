865bc0d9d75917eaee04beed6b7a23bb0df609d9
Owen Luke <hi.owen.luke@gmail.com>
2019-12-25 15:16:45 +0800

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,3 +1,21 @@
 
-const spawn = require('child_process').spawn;
-spawn('node', [__dirname + '/../examples/01-test-web-app/test'], {stdio: "inherit"});
+const fs = require('fs');
+const path = require('path');
+const cp = require('child_process');
+
+const main = () => {
+	const examplesPath = path.resolve(__dirname, '../examples');
+	const exampleNames = fs.readdirSync(examplesPath);
+
+	exampleNames.forEach(exampleName => {
+		if (exampleName.substr(0, 1) === '.') return;
+
+		const examplePath = examplesPath + '/' + exampleName;
+		if (!fs.statSync(examplePath).isDirectory()) return;
+
+		const testPath = examplePath + '/test';
+		cp.spawnSync('node', [testPath], {stdio: "inherit"});
+	});
+};
+
+main();
