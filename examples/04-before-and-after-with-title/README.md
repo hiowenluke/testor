308525605b610b5ba2c8f308707a717a7748c63e
Owen Luke <hi.owen.luke@gmail.com>
2019-12-25 16:43:16 +0800

    add examples #04 before and after with title

---
+++ examples/04-before-and-after-with-title/README.md
@@ -0,0 +1,8 @@
+
+# Run test
+
+```sh
+cd /path/to/testor
+cd examples/03-before-and-after
+node test
+```
---
+++ examples/04-before-and-after-with-title/test/cases.js
@@ -0,0 +1,57 @@
+
+const me = [
+
+	'register',
+	'/user/register?username=owen&password=123',
+	{
+		before: 'kill',
+
+		verify(result) {
+			return result.data > 0;
+		}
+	},
+
+	'get',
+	'/user/get?username=owen',
+	{
+		verify(result) {
+			return result.data.username === 'owen';
+		}
+	},
+
+	'login',
+	'/user/login?username=owen&password=123',
+	{
+		before: 'register',
+		after: 'kill',
+
+		verify(result) {
+			return result.data === true;
+		}
+	},
+
+	'logout',
+	'/user/logout?username=owen',
+	{
+		before: [
+			'register',
+			'login',
+		],
+
+		resultUrl: 'get',
+
+		verify(result) {
+			return result.data.isOnline === 0;
+		}
+	},
+
+	'kill',
+	'/user/kill?username=owen',
+	{
+		verify(result) {
+			return result.data === 1;
+		}
+	},
+];
+
+module.exports = me;
