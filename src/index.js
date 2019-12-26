fb233932d23ba53affa87ee2450f3dab18c69c85
Owen Luke <hi.owen.luke@gmail.com>
2019-12-26 16:22:57 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,63 +1,112 @@
 
 const fs = require('fs');
 const path = require('path');
+const kdo = require('kdo');
 
-const spawn = require('child_process').spawn;
-const caller = require('caller');
+const cp = require('child_process');
 const fx = require('fs-extra');
 
 const config = require('./config');
 const nmpath = require('nmpath');
 
+const tempPath = path.resolve(__dirname, '../.temp');
+const sourceFolderPath = path.resolve(__dirname, './template');
+
+const testorCliOptions = [];
 const mochaCliOptions = [];
+let appServerPath = '';
 
-const parseUserConfig = (userConfig) => {
-	Object.assign(config.server, userConfig);
-};
+const init = {
+	parseCliOptions() {
+		const options = process.argv.slice(2);
+		options.forEach(opt => {
+			if (opt.indexOf('--logs') >= 0 || opt.indexOf('--config') >= 0) {
+				testorCliOptions.push(opt);
+			}
+			else if (/^[./]/.test(opt)) {
+				appServerPath = opt;
+			}
+			else {
+				mochaCliOptions.push(opt);
+			}
+		})
+	},
 
-const parseCliOptions = () => {
-	const args = process.argv.slice(2);
-	args.forEach(arg => {
-		if (arg.indexOf('logs') >= 0) {
-			config.args.logs = true;
+	setAppServerPath() {
+		const getPathOfWorkDir = () => {
+			const child = cp.execSync('pwd');
+			const stdout = child.toString();
+			const result = stdout.split('\n')[0];
+			return result;
+		};
+
+		const pwd = getPathOfWorkDir();
+
+		if (!appServerPath || appServerPath === '.') {
+			appServerPath = pwd;
 		}
 		else {
-			mochaCliOptions.push(arg);
+			switch(appServerPath.substr(0, 1)) {
+				case '.':
+					appServerPath = path.resolve(pwd, appServerPath);
+					break;
+				case '/':
+					// do nothing
+					break;
+			}
 		}
-	})
-};
+	},
 
-const createTempFolder = (tempPath) => {
-	if (!fs.existsSync(tempPath)) {
-		fs.mkdirSync(tempPath)
+	setConfig() {
+		testorCliOptions.forEach(opt => {
+			if (opt.indexOf('--logs') >= 0) {
+				config.cliOptions.logs = true;
+			}
+			else if (opt.indexOf('--config') >= 0) {
+				config.cliOptions.userConfigFile = opt.split('=')[1] || './config.js';
+			}
+		})
+	},
+
+	applyUserConfig() {
+		const userConfigFile = config.cliOptions.userConfigFile;
+		if (userConfigFile) {
+			const filePath = path.resolve(appServerPath, userConfigFile);
+			const userConfig = require(filePath);
+			Object.assign(config.server, userConfig);
+		}
+	},
+
+	createTempFolder() {
+		if (!fs.existsSync(tempPath)) {
+			fs.mkdirSync(tempPath)
+		}
 	}
 };
 
 const updateTemplateFiles = {
-	indexJs(destFolderPath, appTestPath, serverPath) {
+	indexJs({destFolderPath, appTestPath}) {
 		const destFile = destFolderPath + '/index.js';
 
-		const serverFolderName = path.basename(serverPath);
+		const serverFolderName = path.basename(appServerPath);
 		const title = serverFolderName.replace(/-/g, ' ');
 
 		const content = fs.readFileSync(destFile, 'utf-8');
 		const newContent = content
-			.replace('{appTestPath}', appTestPath)
 			.replace('{title}', title)
-			.replace('{serverPath}', serverPath)
-			.replace(`'{serverConfig}'`, JSON.stringify(config.server))
-			.replace(`'{cliArgs}'`, JSON.stringify(config.args))
+			.replace('{appServerPath}', appServerPath)
+			.replace(`'{appServerConfig}'`, JSON.stringify(config.server))
+			.replace(`'{cliOptions}'`, JSON.stringify(config.cliOptions))
 		;
 
 		fs.writeFileSync(destFile, newContent, 'utf-8');
 	},
 
-	createTestsJs(destFolderPath, appTestPath, node_modules) {
+	createTestsJs({destFolderPath, node_modules}) {
 		const destFile = destFolderPath + '/createTests.js';
 
 		const content = fs.readFileSync(destFile, 'utf-8');
 		const newContent = content
-			.replace('{appTestPath}', appTestPath)
 			.replace(`'lodash'`, `'${node_modules}/lodash'`)
 			.replace(`'chai'`, `'${node_modules}/chai'`)
 		;
@@ -65,12 +114,12 @@ const updateTemplateFiles = {
 		fs.writeFileSync(destFile, newContent, 'utf-8');
 	},
 
-	testJs(destFolderPath, appTestPath, node_modules) {
+	testJs({destFolderPath, node_modules}) {
 		const destFile = destFolderPath + '/test.js';
 
 		const content = fs.readFileSync(destFile, 'utf-8');
 		const newContent = content
-			.replace('{appTestPath}', appTestPath)
+			.replace('{appTestPath}', appServerPath + '/test')
 			.replace(`'lodash'`, `'${node_modules}/lodash'`)
 			.replace(`'request'`, `'${node_modules}/request'`)
 			.replace(`'qs'`, `'${node_modules}/qs'`)
@@ -80,29 +129,18 @@ const updateTemplateFiles = {
 	}
 };
 
-const fn = (userConfig = {}) => {
-	parseUserConfig(userConfig);
-	parseCliOptions();
-
-	const tempPath = path.resolve(__dirname, '../.temp');
-	createTempFolder(tempPath);
-
-	const pathToCaller = caller();
-	const appTestPath = path.resolve(pathToCaller, '..');
-	const appServerPath = path.resolve(appTestPath, '..');
+const fn = () => {
+	kdo.doSync(init);
 
 	const appName = path.basename(appServerPath);
-	const sourceFolderPath = path.resolve(__dirname, './template');
 	const destFolderPath = tempPath + '/' + appName;
 	fx.copySync(sourceFolderPath, destFolderPath);
 
 	const node_modules = nmpath(appServerPath);
-	updateTemplateFiles.indexJs(destFolderPath, appTestPath, appServerPath);
-	updateTemplateFiles.createTestsJs(destFolderPath, appTestPath, node_modules);
-	updateTemplateFiles.testJs(destFolderPath, appTestPath, node_modules);
+	kdo.doSync(updateTemplateFiles, {destFolderPath, node_modules});
 
 	const mochaFile = node_modules + "/mocha/bin/mocha";
-	spawn('node', [mochaFile, destFolderPath, ...mochaCliOptions], {stdio: "inherit"});
+	cp.spawn('node', [mochaFile, destFolderPath, ...mochaCliOptions], {stdio: "inherit"});
 };
 
 module.exports = fn;
