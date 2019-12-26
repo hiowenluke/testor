ab03e4614b089ee2f66282c192295332ded8110e
Owen Luke <hi.owen.luke@gmail.com>
2019-12-26 11:56:10 +0800

    Update index.js

--- src/template/index.js
+++ src/template/index.js
@@ -8,6 +8,7 @@ const testCasesDefs = require(appTestPath + '/cases');
 const title = '{title}';
 const serverPath = '{serverPath}';
 const serverConfig = '{serverConfig}';
+const cliArgs = '{cliArgs}';
 
 const wait = (ms = 1000) => {
 	return new Promise(resolve => {
@@ -19,7 +20,8 @@ describe(title, () => {
 	let cp;
 
 	before(async () => {
-		cp = spawn('node', [serverPath]);
+		const options = cliArgs.logs ? {stdio: 'inherit'} : {};
+		cp = spawn('node', [serverPath], options);
 		await wait();
 	});
 
