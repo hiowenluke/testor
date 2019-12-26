e9618d54841706b3acf98c7cbbabb22ed8ade7e0
Owen Luke <hi.owen.luke@gmail.com>
2019-12-26 16:23:01 +0800

    Update index.js

--- src/template/index.js
+++ src/template/index.js
@@ -2,13 +2,12 @@
 const spawn = require('child_process').spawn;
 const createTests = require('./createTests');
 
-const appTestPath = '{appTestPath}';
-const testCasesDefs = require(appTestPath + '/cases');
-
 const title = '{title}';
-const serverPath = '{serverPath}';
-const serverConfig = '{serverConfig}';
-const cliArgs = '{cliArgs}';
+const appServerPath = '{appServerPath}';
+const appServerConfig = '{appServerConfig}';
+const cliOptions = '{cliOptions}';
+
+const testCasesDefs = require(appServerPath + '/test/cases');
 
 const wait = (ms = 1000) => {
 	return new Promise(resolve => {
@@ -20,8 +19,8 @@ describe(title, () => {
 	let cp;
 
 	before(async () => {
-		const options = cliArgs.logs ? {stdio: 'inherit'} : {};
-		cp = spawn('node', [serverPath], options);
+		const options = cliOptions.logs ? {stdio: 'inherit'} : {};
+		cp = spawn('node', [appServerPath], options);
 		await wait();
 	});
 
@@ -29,5 +28,5 @@ describe(title, () => {
 		process.kill(cp.pid, 'SIGTERM');
 	});
 
-	createTests(serverConfig, testCasesDefs);
+	createTests(appServerConfig, testCasesDefs);
 });
